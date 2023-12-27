76cf05a8a4a48139c1c1efcef0fdb3c449516a48
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-12-27 15:00:52 -0300

    primeiro passos com js

---
+++ 2_operadores/scripts.js
@@ -0,0 +1,22 @@
+// 1 - Nurber
+console.log(`O tipo é: ${typeof 2}`)
+console.log(`O tipo é: ${typeof 2.5}`)
+console.log(`O tipo é: ${typeof -2}`)
+
+// 2 - Ops. ariméticas
+console.log(2 + 5)
+console.log(2 * 5)
+console.log(2 / 5)
+console.log(2 ** 5)
+console.log(2 % 5)
+
+console.log(2 + 3 * 2)
+
+// 3 - Special numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * 'coffe')
+
+// 4 - 
+console.log( typeof 'Um testo')
+console.log( typeof `Um testo`)
