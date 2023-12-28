c6e8c9744746f93f4f430f0331ba02e67fc274b7
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-12-28 14:25:07 -0300

    type of datas

--- 2_operadores/scripts.js
+++ 2_operadores/scripts.js
@@ -1,3 +1,4 @@
+/*
 // 1 - Nurber
 console.log(`O tipo é: ${typeof 2}`)
 console.log(`O tipo é: ${typeof 2.5}`)
@@ -17,6 +18,20 @@ console.log(typeof Infinity)
 console.log(typeof -Infinity)
 console.log(12 * 'coffe')
 
-// 4 - 
+// 4 - Strings
 console.log( typeof 'Um testo')
-console.log( typeof `Um testo`)
+console.log( typeof `Um testo`)*/
+
+// 5 - Símbolos especiais em string
+console.log('Um quebra de \nlinha')
+console.log('Tab nas \tstings')
+
+// 6 - Concatenação
+console.log('Hi, ' + "how's " + 'it ' + 'going?')
+
+// Intepolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+// Booleans
+console.log(true)
+console.log(5 > 6)
