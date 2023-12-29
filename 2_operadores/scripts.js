f5676de9d1bc90b9727738f3d9e0ff163f47555c
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-12-29 14:59:31 -0300

    operadores

--- 2_operadores/scripts.js
+++ 2_operadores/scripts.js
@@ -20,7 +20,7 @@ console.log(12 * 'coffe')
 
 // 4 - Strings
 console.log( typeof 'Um testo')
-console.log( typeof `Um testo`)*/
+console.log( typeof `Um testo`)
 
 // 5 - Símbolos especiais em string
 console.log('Um quebra de \nlinha')
@@ -29,9 +29,37 @@ console.log('Tab nas \tstings')
 // 6 - Concatenação
 console.log('Hi, ' + "how's " + 'it ' + 'going?')
 
-// Intepolação
+// 7 - Intepolação
 console.log(`A soma de 2 + 2 é: ${2 + 2}`)
 
-// Booleans
+// 8 - Booleans
 console.log(true)
 console.log(5 > 6)
+console.log(1 < 5)
+
+// 9 - conparação
+console.log(5 <= 5)
+console.log(5 < 5)
+console.log(10 == 10)
+console.log(10 != 8)
+
+// 10 - Idêtico
+console.log(9 == '9')
+console.log(9 === '9')
+console.log(9 != '9')
+console.log(9 !== '9')
+
+// 11 - Operadores lógica
+console.log(true && false)
+console.log(false || false)
+console.log(!false)
+
+console.log(5 > 2 && 2 < 10)
+console.log(5 < 1 || 2 === 2)
+console.log(!5 > 2)*/
+
+// 12 - empty values
+console.log(typeof null, typeof undefined)
+console.log(null == undefined)
+console.log(null == false)
+console.log(undefined == false)
