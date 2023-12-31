a4782e428448b67b958d70676ec0f6a27c4a4ff1
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2023-12-31 09:41:18 -0300

    estrutura de código

--- 3_estruturas/js/scripts.js
+++ 3_estruturas/js/scripts.js
@@ -1,5 +1,5 @@
 // 1 - Variáveis
-let nome = 'Elson'
+/*let nome = 'Elson'
 nome = 'Elson Santos'
 console.log(nome)
 
@@ -21,19 +21,66 @@ let _test = 'ok'
 let $test = 'ok'
 console.log(_test, $test)
 
-// prompt
+// 3 - prompt
 // let age = window.prompt('Digite sua idade? ')
 // console.log(age)
 
+// 4 - alert
 // const num = 10
 // window.alert('testing ' + num)
 
-// Manth
+// 5 - Manth
 console.log(Math.max(3, 6, 1, 5))
 console.log(Math.min(3, 6, 1, 5))
 console.log(Math.ceil(5.5))
 console.log(Math.floor(5.5))
 console.log(Math.sqrt(9))
 
-// console
+// 6 - console
 console.log(console.error('mistake'))
+console.log(console.warn(`Aviso`))*/
+
+// 7 - if
+const m = 10
+if(m > 5) {
+    console.log(`${m} maior que 5!`)
+}
+
+const user = 'João'
+if(user === 'João') {
+    console.log(`Hello, ${user}!`)
+}
+
+if(user === 'Maria') {
+    console.log('Hello, Maria!')
+}
+console.log(user === 'João', user === 'Maria')
+
+// 8 - else
+const loggedIn = false
+if(loggedIn) {
+    console.log(`Está autenticado!`)
+} else {
+    console.log(`Os número não são mais altos`)
+}
+
+// 9 else if
+if(1 > 2) {
+    console.log('test')
+}
+else if(2 > 3) {
+    console.log('test 2')
+}
+else if (5 > 1) {
+    console.log('Agoar sim!')
+}
+
+const userName = 'Elson'
+const userAge = 21
+if(userName === 'José') {
+    console.log('Bem-vindo José!')
+} else if(userName === 'Elson' && userAge === 21) {
+    console.log(`Olá ${userName}, você tem ${userAge} anos!`)
+} else {
+    console.log('Nenhau condição aceita!')
+}
