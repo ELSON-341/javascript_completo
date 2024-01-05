d57d0aabd871f8505cce97bcf6f27ec80dfd21af
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-01-05 14:53:20 -0300

    arrow function

--- 4_funcoes/script.js
+++ 4_funcoes/script.js
@@ -27,3 +27,46 @@ function soma(n1, n2) {
 
 const retorna = soma(num, num1)
 console.log(retorna)
+
+// 4 - escopo aninhado
+let m = 10
+
+function escopoAninhado() {
+    let = 20
+    
+    if(true) {
+        let m = 30
+        
+        if(true) {
+            let m = 40
+
+            console.log(m)
+        }
+
+        console.log(m)
+    }
+
+    console.log(m)
+}
+
+escopoAninhado()
+console.log(m)
+
+// arrow fubction
+const testArrow = () => {
+    console.log('Testing')
+}
+
+const evenOrOdd = (n=0) => {
+    if(n % 2 == 0){
+        return console.log(`O número ${n} é par.`)
+    } else {
+        return console.log(`O número ${n} é ímpar.`)
+    }
+}
+
+evenOrOdd(6)
+
+const squareRoot = (x) => x * x
+
+console.log(squareRoot(4))
