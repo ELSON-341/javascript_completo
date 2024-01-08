a48a4d1ba92e345fbce404ca7877f4dd02a8f1a2
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-01-08 14:56:54 -0300

    object and arrays

--- 5_arrays_e_objetos/script.js
+++ 5_arrays_e_objetos/script.js
@@ -17,4 +17,41 @@ console.log(numbers['length'])
 const myName = 'Elson'
 console.log(myName['length'])
 
-// métados
+// 4 - métados
+const otherNumbers = [1, 2, 3]
+const allNumbers = numbers.concat(otherNumbers)
+allNumbers.push()
+allNumbers.concat()
+allNumbers.pop()
+console.log(allNumbers)
+
+const text = 'test text'
+console.log(text.toUpperCase())
+console.log(text.toLowerCase())
+
+console.log(text.indexOf('x'))
+
+// 5 - objects
+const person = {
+    name: 'Elson',
+    age: 21,
+    job: 'Programador'
+}
+console.log(person)
+console.log(person.name)
+console.log(person.name.length)
+console.log(typeof person)
+
+// 6 - criando e deletando propriedades
+const car = {
+    engine: 2.0,
+    brand: 'BMW',
+    km: 2000
+}
+
+console.log(car)
+car.doors = 4
+console.log(car)
+delete car.km
+console.log(car)
+console.log(car.doors)
