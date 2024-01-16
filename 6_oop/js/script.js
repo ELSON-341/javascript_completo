1259e8d0edfce4e05b987477eebaf0b90f0badaa
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-01-16 15:16:12 -0300

    studying prototype

--- 6_oop/js/script.js
+++ 6_oop/js/script.js
@@ -16,6 +16,7 @@ const pessoas = {
         return this.nome
     },
 
+
     setNome: function(novoNome) {
         this.nome = novoNome
     }
@@ -24,3 +25,27 @@ console.log(pessoas.nome);
 console.log(pessoas.getNome());
 pessoas.setNome('Matheus')
 console.log(pessoas.getNome());
+
+// 3 - prototype
+const str = 'test'
+console.log(Object.getPrototypeOf(str))
+
+const bool = true
+console.log(Object.getPrototypeOf(bool))
+
+const arr = []
+console.log(arr.length)
+console.log(Object.getPrototypeOf(arr))
+
+// 4 - mais sobre prototype
+const myObject = {
+    a: 'b'
+}
+
+console.log(Object.getPrototypeOf(myObject))
+console.log(Object.getPrototypeOf(myObject) === Object.prototype)
+
+const mySecondoObject = Object.create(myObject)
+console.log(mySecondoObject)
+console.log(mySecondoObject.a)
+console.log(Object.getPrototypeOf(mySecondoObject) === myObject)
