c7253cc0ea2db24f88a4c77b45ffbc65f7fb0a6d
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-01-21 14:36:46 -0300

    debugs

---
+++ 7_debug/js/script.js
@@ -0,0 +1,26 @@
+// 1 - strict
+"use strict"
+// opa = 'test'
+// const opa = 'testing'
+// const undefined = 10
+// delete [].length
+
+// 2 - console.log
+let a = 1 
+let b = 2
+if(a == 1) {
+    a = b + 2
+}
+console.log(a)
+
+for(let i = 0; i < b; i++) {
+    a = a + 2
+    console.log(a);
+}
+
+if(2 > 5) {
+    a = 25
+}
+console.log(a);
+
+
