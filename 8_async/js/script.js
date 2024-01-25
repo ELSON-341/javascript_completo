6761394aec3a7559b4076af5f59bf7e7e103ad2e
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2024-01-25 09:47:21 -0300

    working with promise

--- 8_async/js/script.js
+++ 8_async/js/script.js
@@ -7,3 +7,25 @@ setTimeout(function() {
 }, 2000)
 
 console.log(`Ainda não executou 2`);
+
+// 2 - setInterval
+console.log(`Ainda não começou`)
+
+// setInterval(function() {
+//     console.log(`Requisição assíncrona`)
+// }, 3000)
+
+console.log(`Ainda não começou`)
+
+// 3 - Promises
+const promessa = Promise.resolve(5 + 5)
+
+console.log('Algum código')
+
+promessa.then(value => {
+    console.log(`A soma é ${value}`)
+    return value
+})
+.then((value) => value - 1)
+promessa.then((value) => console.log(`Agora é ${value}`))
+
