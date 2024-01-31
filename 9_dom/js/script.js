ecf93689b36350e8aefebbd5dca1941db0372e98
ELSON-341 <elson.sn314@gmail.com>
2024-01-31 14:58:46 -0300

    selector element

--- 9_dom/js/script.js
+++ 9_dom/js/script.js
@@ -1,5 +1,23 @@
 // 1 - movendo-se pelo DOM
-console.log(document.body)
-console.log(document.body.childNodes[1])
-console.log(document.body.childNodes[1].childNodes)
-console.log(document.body.childNodes[1].childNodes[1].textContent)
+console.log(window.document.body);
+console.log(window.document.body.childNodes[1]);
+console.log(window.document.body.childNodes[1].childNodes[1].textContent)
+
+// 2 - selecionando por tag
+const listItens = window.document.getElementsByTagName('li')
+console.log(listItens)
+
+// 3 - selecinando por id
+const title = window.document.getElementById('title')
+console.log(title);
+
+// 4 - selecinando por class
+const products = window.document.getElementsByClassName('product')
+console.log(products)
+
+// 5 - selecinando os elementos por CSS
+const produtsQury = window.document.querySelectorAll('.product')
+console.log(produtsQury);
+
+const mainContainer = window.document.querySelector('#main-container')
+console.log(mainContainer);
