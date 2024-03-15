edf1c03014c10013741935131e6121899c338d4b
ELSON-341 <elson.sn314@gmail.com>
2024-03-15 15:23:11 -0300

    studying axios

--- 12_axios/js/script.js
+++ 12_axios/js/script.js
@@ -1,14 +1,43 @@
-'use strict'
+'user strict'
 // 1 - instalação
 console.log(axios);
 
 // 2 - first request
 const getData = async () => {
     try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+        
+        console.log(response);
+        return response.data
     } catch(error) {
-        console.log(error);
+        console.log(`Houve um erro: ${error}`);
     }
 }
 
 getData()
+
+// 3 - imprimendo dados na tela
+const container = document.getElementById('user-container')
+
+const printdata = async () => {
+    const data = await getData()
+
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement('div')
+
+        const nameElement = document.createElement('h2')
+        nameElement.textContent = user.name
+        
+        const emailElement = document.createElement('p')
+        emailElement.textContent = user.email
+
+        div.appendChild(nameElement)
+        div.appendChild(emailElement)
+        container.appendChild(div)
+
+    });
+}
+
+printdata()
