4b1302f1ff4d3bf21f4ce439aebf1c1f30601b83
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-08-02 09:56:12 -0300

    Opradores lógicos

--- 2_operadores/scripts.js
+++ 2_operadores/scripts.js
@@ -1,67 +1,84 @@
 // 1- Number
-console.log(typeof 2)
-console.log(typeof 2.66)
-console.log(typeof -35)
-console.log(typeof -56.88)
+// console.log(typeof 2)
+// console.log(typeof 2.66)
+// console.log(typeof -35)
+// console.log(typeof -56.88)
 
 // 2 - ops. aritmético
-console.log(2 + 4)
-console.log(10 + 5)
-console.log(4 * 5)
-console.log(4 / 2)
-console.log(2 ** 3)
-console.log(9 % 3)
-console.log((5 + (4 *2)))
+// console.log(2 + 4)
+// console.log(10 + 5)
+// console.log(4 * 5)
+// console.log(4 / 2)
+// console.log(2 ** 3)
+// console.log(9 % 3)
+// console.log((5 + (4 *2)))
 
 // 3 - Special Number
-console.log(typeof Infinity)
+// console.log(typeof Infinity)
 
-console.log(typeof -Infinity)
+// console.log(typeof -Infinity)
 
-console.log(2 * 'asd')
+// console.log(2 * 'asd')
 
-console.log(typeof NaN)
+// console.log(typeof NaN)
 
 // 4 - Strings
-console.log('Um texto')
-console.log("Mais um texto")
-console.log(`outro texto`)
-console.log(typeof 'Um texto')
+// console.log('Um texto')
+// console.log("Mais um texto")
+// console.log(`outro texto`)
+// console.log(typeof 'Um texto')
 
 // 5 - Símbolos especias em strigs
-console.log("Testondo a \nquebra de linha")
-console.log("Espaço de \tTAB")
+// console.log("Testondo a \nquebra de linha")
+// console.log("Espaço de \tTAB")
 
 // 6 - Concatenação
-console.log('Oi, ' + 'tudo ' + 'bem?')
+// console.log('Oi, ' + 'tudo ' + 'bem?')
 
 // 7 - Templete Strings
-console.log(`A soma de 2 + 2 = ${2 + 2}`)
-console.log(`Podemos executar qualquer coisa ${console.log('teste')}`)
+// console.log(`A soma de 2 + 2 = ${2 + 2}`)
+// console.log(`Podemos executar qualquer coisa ${console.log('teste')}`)
 
 // 8 - boolean
-console.log(true)
+// console.log(true)
 
-console.log(2 > 10)
+// console.log(2 > 10)
 
-console.log(2 > 1)
+// console.log(2 > 1)
 
-console.log(typeof false)
+// console.log(typeof false)
 
 // 9 - Comparações
-console.log(5 <= 5)
+// console.log(5 <= 5)
 
-console.log(10 == 10)
+// console.log(10 == 10)
 
-console.log(10 == 9)
+// console.log(10 == 9)
 
-console.log(10 === '10')
+// console.log(10 === '10')
 
-console.log(10 != 9)
+// console.log(10 != 9)
 
 // 10 - Idêntico
-console.log(9 == '9')
+// console.log(9 == '9')
 
-console.log(9 === '9')
+// console.log(9 === '9')
 
-console.log(9 !== '9')
+// console.log(9 !== '9')
+
+// 11 - Operadores lógicos
+console.log(true && true)
+
+console.log(true && false)
+
+console.log(1 > 2 && 2 < 10)
+
+console.log(5 > 2 && 'Elson' === 1)
+
+console.log(5 > 2 || 'Elson' === 1)
+
+console.log(5 < 2 || 5 > 100)
+
+console.log(!true)
+
+console.log(!5 > 2)
