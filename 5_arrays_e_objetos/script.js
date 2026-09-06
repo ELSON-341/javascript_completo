7b834dda0a89e43c92bd692b22370b53889cf4a1
ELSON-341 <130087373+ELSON-341@users.noreply.github.com>
2026-09-06 09:13:23 -0300

    Object

--- 5_arrays_e_objetos/script.js
+++ 5_arrays_e_objetos/script.js
@@ -33,4 +33,51 @@ console.log(allNumbers)
 const text = 'Alguma texto'
 
 console.log(typeof text.toUpperCase)
-console.log(typeof text.indexOf)
+console.log(typeof text.indexOf) 
+
+// 5 - Objetos
+const person = {
+    name: 'Elson',
+    age: 24,
+    jod: 'Programador'
+}
+
+console.log(person)
+
+console.log(person.name)
+
+console.log(person.name.length)
+console.log(typeof person)
+
+// 6 - criando e deletando propriedades
+const car = {
+    engine: 2.0,
+    brand: 'VW',
+    km: '2000'
+}
+
+console.log(car)
+
+car.door = 4
+
+console.log(car)
+
+delete car.km
+
+console.log(car)
+
+// 7 - mais sobre ojetos
+const obj = {
+    a: 'teste',
+    b: true
+}
+
+console.log(obj instanceof Object)
+
+const obj2 = {
+    c:[]
+}
+
+Object.assign(obj2, obj)
+
+console.log(obj2);
